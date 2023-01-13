9953edb6e66fc517f8d4bdfa486a2576196f87a1
EUAHI <122543700+EUAHI@users.noreply.github.com>
2023-01-13 09:54:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("D D AB Fb G F D ", 160)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.showIcon(IconNames.Heart)
