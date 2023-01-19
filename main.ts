f449103b86cc7ca2463630725560ea71edcb1393
EUAHI <122543700+EUAHI@users.noreply.github.com>
2023-01-19 09:56:14 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,33 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.No)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
-    music.playMelody("D D AB Fb G F D ", 160)
+    music.playMelody("B A G A G F A C5 ", 60)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.SmallHeart)
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Confused)
 })
 basic.showIcon(IconNames.Heart)
