51ddfb77e011cf941a735c2f286d0a122eb449f3
EUAHI <122543700+EUAHI@users.noreply.github.com>
2023-01-20 09:54:26 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,8 +8,20 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         `)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showString("Wow!")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.EigthNote)
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+    basic.showIcon(IconNames.Asleep)
+})
 input.onButtonPressed(Button.AB, function () {
-    music.playMelody("B A G A G F A C5 ", 60)
+    music.playMelody("G E C C E - G - ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 10; index++) {
@@ -20,14 +32,19 @@ input.onButtonPressed(Button.B, function () {
             . # # # .
             # . # . #
             `)
-        basic.pause(100)
         basic.clearScreen()
     }
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoDown, function () {
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.Skull)
 })
+basic.showString("Hello!")
 basic.showIcon(IconNames.Heart)
